d73062c2e3c84d205eb762ab3c113e1ea3393074
XinYu101 <gexinyu101@126.com>
2017-10-31 13:56:56 +0800

    name : test two user : gexiny101

---
+++ src/main/resources/entity/GrzxJrjgm.java
@@ -0,0 +1,26 @@
+package com.prod.entity.sxzx;
+
+/**
+ * @Author: gxy
+ * @Date: Created by as on 2017/10/20.
+ */
+public class GrzxJrjgm {
+    String grJrjgdm;
+    String queryOrgNo;
+
+    public String getGrJrjgdm() {
+        return grJrjgdm;
+    }
+
+    public void setGrJrjgdm(String grJrjgdm) {
+        this.grJrjgdm = grJrjgdm;
+    }
+
+    public String getQueryOrgNo() {
+        return queryOrgNo;
+    }
+
+    public void setQueryOrgNo(String queryOrgNo) {
+        this.queryOrgNo = queryOrgNo;
+    }
+}
